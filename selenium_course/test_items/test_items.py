9e9bd62fa93e19d4ba4db18c0748e79fc81b563b
Oleg Mikheenko <o.mikheenko@mail.ru>
2019-09-10 18:55:03 +0300

    change method name test_items

--- selenium_course/test_items/test_items.py
+++ selenium_course/test_items/test_items.py
@@ -1,9 +1,10 @@
-
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_add_to_chart_button_found(browser):
     browser.get(link)
+    time.sleep(10)
     add_to_chart_btn = browser.find_elements_by_css_selector("#add_to_basket_form > button")
     assert len(add_to_chart_btn) > 0, "There is no 'Add to chart' button"
